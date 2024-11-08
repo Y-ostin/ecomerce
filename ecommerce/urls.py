d037439d63e517006e8a27371cd7c2143708847b
Y-ostin <yostin.arequipa@tecsup.edu.pe>
2024-11-07 23:18:34 -0500

    finish web

---
+++ ecommerce/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
+from django.contrib import admin
+
+
+urlpatterns = [
+                  path('users/', include('users.urls')),
+                  path('admin/', admin.site.urls), 
+                  path('shop/', include('store.urls')),
+                  path('', RedirectView.as_view(pattern_name='home', permanent=False))
+
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
